bd125dd25fa18e2266e18d8fe22739ae4cdb8d2b
Alexandre Bertho <46300952+alexbertho@users.noreply.github.com>
2025-04-07 20:05:07 +0000

    restruct memory to print

--- include/allocateur.h
+++ include/allocateur.h
@@ -1,8 +1,13 @@
+#ifndef ALLOCATEUR_H 
+#define ALLOCATEUR_H
+
 #include <stddef.h>
 #include <stdint.h>
 
-#ifndef ALLOCATEUR_H 
-#define ALLOCATEUR_H
+#define TAILLE_MEMOIRE_DYNAMIQUE (1<<15)
+#define TAILLE_BLOC_OCTETS sizeof(bloc) 
+
+typedef uint32_t bloc;
 
 void initialiser_memoire_dynamique();
 void *allocateur_malloc(size_t size);
@@ -10,7 +15,7 @@ void afficher_memoire();
 void afficher_stat_memoire();
 void afficher_stat_memoire_bref();
 int ramasse_miette_lire_marque(void * ptr);
-int ramasse_miette_poser_marque(void * ptr);
+void ramasse_miette_poser_marque(void * ptr);
 void ramasse_miette_liberer();
 void allocateur_free (void * ptr);
 
--- src/memory/allocateur.c
+++ src/memory/allocateur.c
@@ -1,11 +1,9 @@
+#include "allocateur.h"
 #include <stdint.h>
 #include <stdio.h>
 #include <stddef.h>
 #include <stdlib.h>
 
-typedef uint32_t bloc;
-#define TAILLE_MEMOIRE_DYNAMIQUE (1<<15)
-#define TAILLE_BLOC_OCTETS sizeof(bloc)  
 
 bloc MEMOIRE_DYNAMIQUE[TAILLE_MEMOIRE_DYNAMIQUE];
 
@@ -166,60 +164,3 @@ void allocateur_free (void *ptr) {
     allocateur_free_bloc(i);
 
 }
-
-void info_bloc(size_t i) {
-    int t = taille_bloc(i);
-    int p = bloc_precedant(i);
-    int s = bloc_suivant(i);
-    char * u = usage_bloc(i) ? "[x]" : "[ ]";
-
-    printf("%-7s %-18p %7d → %7ld → %-7d (%6d)\n",
-           u, (void *) &MEMOIRE_DYNAMIQUE[i], p, i, s, t);
-}
-
-void afficher_memoire() {
-    int i;
-    printf("%-7s %-7s %-18s %7s → %7s → %-7s %7s\n", 
-           "[Indice]", "Reservé", "Adresse", "Precedent", "Courant", "Suivant", "(Taille)");
-    printf("------------------------------------------------------------------------\n");
-    
-    for (i=0; bloc_suivant(i) != i; i = bloc_suivant(i)) {
-        printf("[%5d] ", i);
-        info_bloc(i);
-    }
-    printf("[%5d] ", i);
-    info_bloc(i);
-}
-
-void afficher_stat_memoire() {
-    size_t i = 0;
-    int t = TAILLE_MEMOIRE_DYNAMIQUE;
-    int n = 0;
-    int b = 0;
-    int a = 0;
-    for (i=0; bloc_suivant(i) != i; i = bloc_suivant(i)) {
-        n+=1;
-        if (usage_bloc(i)) {
-            a += taille_bloc(i) + 1;
-            b += 1;
-        }
-    }
-    printf("%d/%d (%.2f %%) %d/%d blocs alloués\n", a, t, (100.*a)/t, b,n);
-}
-
-void afficher_stat_memoire_bref() {
-    size_t i = 0;
-    int t = TAILLE_MEMOIRE_DYNAMIQUE;
-    int n = 0;
-    int b = 0;
-    int a = 0;
-    for (i=0; bloc_suivant(i) != i; i = bloc_suivant(i)) {
-        n+=1;
-        if (usage_bloc(i)) {
-            a += taille_bloc(i) + 1;
-            b += 1;
-        }
-    }
-    printf("%.2f%%\n", (100.*a)/t);
-}
-
