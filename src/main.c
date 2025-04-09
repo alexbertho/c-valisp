88191b7f7ea3456aebb98231f16708e43080e8e5
Alexandre <46300952+alexbertho@users.noreply.github.com>
2025-04-09 17:01:41 +0200

    Remove my test code to the main.c

--- src/main.c
+++ src/main.c
@@ -8,18 +8,6 @@
 
 
 int main() {
-    sexpr a,b,c,liste;
-
-    initialiser_memoire_dynamique();
-    
-    a = new_integer(5);
-    b = new_integer(3);
-    c = new_integer(2);
-    liste = cons(a, cons(b, cons(c, NULL)));
-    
-    afficher(add_valisp(liste, NULL));
-
-    printf("\n");
     
     return 0;
 }
