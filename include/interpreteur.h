795c92c4b24751d868c3ea502b5481e946d6511f
Alexandre <46300952+alexbertho@users.noreply.github.com>
2025-04-15 13:04:46 +0000

    Fix include conflict

--- include/interpreteur.h
+++ include/interpreteur.h
@@ -8,6 +8,8 @@
 #define INTERPRETEUR_H
 sexpr eval_list(sexpr V, sexpr env);
 sexpr bind(sexpr variables, sexpr liste, sexpr env);
+
 sexpr apply(sexpr fonction, sexpr liste, sexpr env);
 sexpr eval(sexpr val, sexpr env);
+
 #endif
