f32b61f63584eb74554bc42e4a652f6cde4b1e08
Alexandre <46300952+alexbertho@users.noreply.github.com>
2025-04-29 17:26:44 +0200

    Fix include on previous commit

--- src/main.c
+++ src/main.c
@@ -1,10 +1,4 @@
 #include "valisp.h"
-#include "load_file.h"
-#include "allocateur.h"
-#include "memoire.h"
-#include "environnement.h"
-#include "valisp.h"
-
 
 int main(int argc, char *argv[]) {
     return valisp_main(argc, argv);
